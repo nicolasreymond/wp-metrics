ddf6772e13c411312cacae33efc1f91ba0657c31
nicolasreymond <n.reymond.12@gmail.com>
2021-04-20 16:10:36 +0200

    [add] Create the main plugin file.

---
+++ wordpress-exporter-prometheus.php
@@ -0,0 +1,15 @@
+<?php
+
+    /**
+    * Plugin Name: Wordpress Prometheus Exporter
+    * Plugin URI: https://github.com/nicolasreymond/wp-metrics.git
+    * Description:  This Wordpress plugin exports metrics for prometheus to scrape 'em 
+    * Author: Nicolas Reymond
+    * Author URI: https://github.com/nicolasreymond
+    * Version: 0.1
+    */
+     
+    function my_awesome_func( $data ) {
+        return "";
+    }
+    
