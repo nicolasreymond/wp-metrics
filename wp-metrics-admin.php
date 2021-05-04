b6342e2661c34bde827a028a2113b6b7a63bd337
uid167114 <uid167114@h2web240.infomaniak.ch>
2021-05-04 13:48:48 +0200

    [feature] Now it's possible to change the metrics pass

--- wp-metrics-admin.php
+++ wp-metrics-admin.php
@@ -1,13 +1,74 @@
 <?php
 
-add_action('admin_menu', 'test_plugin_setup_menu');
- 
-function test_plugin_setup_menu(){
-    add_menu_page( 'Test Plugin Page', 'Test Plugin', 'manage_options', 'test-plugin', 'test_init' );
-}
- 
-function test_init(){
-    echo "<h1>Hello World!</h1>";
-}
- 
-?>
+
+class MyPlugin{  
+    
+      private $my_plugin_screen_name;  
+      private static $instance;  
+    
+      static function GetInstance()  
+      {  
+            
+          if (!isset(self::$instance))  
+          {  
+              self::$instance = new self();  
+          }  
+          return self::$instance;  
+      }  
+        
+      public function PluginMenu()  
+      {  
+       $this->my_plugin_screen_name = add_menu_page(  
+                                        'My Plugin',   
+                                        'My Plugin',   
+                                        'manage_options',  
+                                        __FILE__,   
+                                        array($this, 'RenderPage'),   
+                                        plugins_url('/img/icon.png',__DIR__)  
+                                        );  
+      }  
+        
+      public function RenderPage(){  
+       ?>  
+       <div class='wrap'>  
+        <h2>custom form</h2>  
+        <div class="main-content">  
+  
+  
+        <form class="form-basic" method="post" action="#">  
+  
+            <div class="form-title-row">  
+                <h1>Administration</h1>  
+            </div>  
+  
+            
+            <div class="form-row">  
+                <label>  
+                    <span>Set password</span>  
+                    <input type="text" name="password">  
+                </label>  
+            </div>  
+            
+  
+            <div class="form-row">  
+                <button type="submit">Submit Form</button>  
+            </div>  
+  
+        </form>  
+  
+    </div>  
+          
+       </div>  
+       <?php  
+      }  
+  
+      public function InitPlugin()  
+      {  
+           add_action('admin_menu', array($this, 'PluginMenu'));  
+      }  
+    
+ }  
+   
+$MyPlugin = MyPlugin::GetInstance();  
+$MyPlugin->InitPlugin();  
+?> 
